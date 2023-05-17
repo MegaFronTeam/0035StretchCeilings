e3f8b198bf500a98d6da6bdf7b6dcbf55cb2cb13
Janis <janis.paberzs18@gmail.com>
2023-05-17 21:36:05 +0300

    add sorting

--- newEdits/newScripts2.js
+++ newEdits/newScripts2.js
@@ -12,6 +12,78 @@ function eventHandler() {
   });
 };
 
+
+function shuffleArray() {
+  let cardsArr = [];
+  let cardsAscSorted = [];
+  let cardsRow = document.querySelector('.cards__row');
+  for (const item of document.querySelectorAll('.card__col')) {
+    cardsArr.push(item);
+  }
+
+  cardsArr.forEach(item => {
+    cardsAscSorted.push({
+      cardItemHtml: item,
+      cardItemPrice: Number(item.querySelector('.catblock_price span').innerHTML),
+    })
+  })
+  cardsAscSorted.sort(
+    function(a, b){
+      return parseFloat(a.cardItemPrice) - parseFloat(b.cardItemPrice);
+    }
+  )
+
+  let currentIndex = cardsArr.length;
+  let temporaryValue, randomIndex;
+
+  let sortByPriceSelcet = document.querySelector('.sort-by-price');
+
+  // Random sorting
+  if (sortByPriceSelcet.value === '---') {
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = cardsArr[currentIndex];
+      cardsArr[currentIndex] = cardsArr[randomIndex];
+      cardsArr[randomIndex] = temporaryValue;
+    }
+    for (const item of cardsRow.children) {
+      item.remove();
+    }
+    for (const item of cardsArr) {
+      cardsRow.insertAdjacentElement('beforeend', item);
+    }
+  }
+
+  // Ascending sorting
+  if (sortByPriceSelcet.value === 'asc') {
+    for (const item of cardsRow.children) {
+      item.remove();
+    }
+    for (const item in cardsAscSorted) {
+      cardsRow.insertAdjacentElement('afterbegin', cardsAscSorted[item].cardItemHtml);
+    }
+  }
+
+  // Descending sorting
+  if (sortByPriceSelcet.value === 'ascend') {
+    for (const item of cardsRow.children) {
+      item.remove();
+    }
+    for (const item in cardsAscSorted) {
+      cardsRow.insertAdjacentElement('beforeend', cardsAscSorted[item].cardItemHtml);
+    }
+  }
+}
+
+let sortBtn = document.querySelector('.choose-ceiling__btn');
+if(sortBtn) {
+  sortBtn.addEventListener('click', () => {
+    shuffleArray();
+  })
+}
+
 if (document.readyState !== 'loading') {
 	eventHandler();
 } else {
